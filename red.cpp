08ebf9b66cc9536999c4360f27b483b1bbb0496d
Diego Granados <diego.granados@udea.edu.co>
2019-09-16 17:10:20 -0500

    Creación clase redes (falta terminar)

--- red.cpp
+++ red.cpp
@@ -2,5 +2,33 @@
 
 Red::Red()
 {
+    char link[45] = {'\0'};
+    ifstream topologiaRouter("red.txt");
+    if(topologiaRouter.is_open())
+    {
+        int cont = 0;
+        while(!topologiaRouter.eof())
+        {
+           link[cont] = topologiaRouter.get();
+           cont++;
+        }
+        topologiaRouter.close();
+        char delimitador[] = "-=";
+        string links = "";
+        //solo en la primer llamada a strtok pasamos cadena en primer argumento
+        char *subStr = strtok(link, delimitador);
+        if(subStr != NULL)
+        { //NULL es para saber hasta donde va a segmentar
+            while(subStr != NULL)
+            {
+                // Sólo en la primera pasamos la cadena; en las siguientes pasamos NULL
+                links += subStr;
+                //En las demás llamadas a strtok le pasamos como primer arg a NULL
+                subStr = strtok(NULL, delimitador);
+            }
+        }
+        cout << links << endl;
 
+    }
+    else cout << "Fichero no existe o faltan permisos para abrirlo" << endl;
 }
--- red.h
+++ red.h
@@ -1,9 +1,16 @@
 #ifndef RED_H
 #define RED_H
 
+#include <iostream>
+#include <string>
+#include <map>
+#include <iterator>
+#include "router.h"
 
 class Red
 {
+private:
+    map <string, Router> redRouters;
 public:
     Red();
 };
